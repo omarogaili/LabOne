013af44c0872bf1117bfd33963c0d3ff288d2fbf
Omar Ogaili <ogailiomar@hotmail.com>
2024-02-28 11:00:39 +0100

    Nature is Done

--- LabEtt_Spel/Spel/ApplicationDb.cs
+++ LabEtt_Spel/Spel/ApplicationDb.cs
@@ -1,11 +1,11 @@
-﻿// using Microsoft.EntityFrameworkCore;
-// namespace Spel;
+﻿using Microsoft.EntityFrameworkCore;
+namespace Spel;
 
-// public class ApplicationDb : DbContext
-// {
-//     public DbSet<User> users { get; set; } // crate an object for the users collection
-//      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//  {
-//      optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Spel_LabEtt"); //can be changed if u have another server. 
-//  }
-// }
+public class ApplicationDb : DbContext
+{
+    public DbSet<User> users { get; set; } // crate an object for the users collection
+     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+ {
+     optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Spel_LabEtt"); //can be changed if u have another server. 
+ }
+}
